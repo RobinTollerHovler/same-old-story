acf0a929e61c8fab44a1e74f2eb6dd70833e87d3
Robin Toller Hovler <robin@colobri.se>
2022-11-08 14:10:18 +0100

    Movie is created (in the background)

--- Assets/Scripts/Movies/Movie.cs
+++ Assets/Scripts/Movies/Movie.cs
@@ -2,6 +2,12 @@ namespace SameOldStory.Movies {
     
     public class Movie {
         
+        public string Name { get; private set; }
+
+        public Movie(string name) {
+            Name = name;
+        }
+
     }
     
 }
---
+++ Assets/Scripts/UI/MovieMaker.cs
@@ -0,0 +1,19 @@
+using SameOldStory.Movies;
+using TMPro;
+using UnityEngine;
+
+namespace SameOldStory.UI {
+    
+    public class MovieMaker : MonoBehaviour {
+
+        [SerializeField] private TMP_InputField movieNameInputField;
+
+        private Movie constructingMovie;
+
+        public void SubmitMovieName() {
+            constructingMovie = new Movie(movieNameInputField?.text);
+        }
+
+    }
+    
+}
