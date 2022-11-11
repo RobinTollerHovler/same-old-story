24993a8d3e63ecb47fda2961a73d76ce6af39eb8
Robin Toller Hovler <robin@colobri.se>
2022-11-11 11:11:43 +0100

    Start writing a movie will add a timeline object

--- Assets/Scripts/UI/Timeline/Timeline.cs
+++ Assets/Scripts/UI/Timeline/Timeline.cs
@@ -1,3 +1,4 @@
+using SameOldStory.Movies;
 using UnityEngine;
 
 namespace SameOldStory.UI.Timeline {
@@ -7,6 +8,32 @@ namespace SameOldStory.UI.Timeline {
         [SerializeField] private GameObject timelineMovieTemplate;
         [SerializeField] private Transform movieNode;
 
+        private RectTransform rectTransform;
+        
+        private void Awake() => rectTransform = GetComponent<RectTransform>();
+
+        private void Update() {
+            //rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, new Vector2(0, 500), Time.deltaTime);
+        }
+
+        private void OnEnable() => Movie.onMovieBeginWriting += AddMovieToTimeline;
+        private void OnDisable() => Movie.onMovieBeginWriting -= AddMovieToTimeline;
+
+        private void AddMovieToTimeline(Movie movie) {
+            if (movieNode == null || timelineMovieTemplate == null) return;
+            Instantiate(timelineMovieTemplate, movieNode);
+            OrganiseMovies();
+        }
+
+        private void OrganiseMovies() {
+            int n = 0;
+            float height = 35;
+            foreach (TimelineMovie timelineMovie in GetComponentsInChildren<TimelineMovie>()) {
+                timelineMovie.SetVertical(n * height);
+                n++;
+            }
+        }
+        
     }
 
 }
--- Assets/Scripts/UI/Timeline/TimelineMovie.cs
+++ Assets/Scripts/UI/Timeline/TimelineMovie.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 namespace SameOldStory.UI.Timeline {
     
     public class TimelineMovie : MonoBehaviour {
+
+        public void SetVertical(float position) {
+            RectTransform rect = GetComponent<RectTransform>();
+            rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, -position);
+        }
         
     }
     
