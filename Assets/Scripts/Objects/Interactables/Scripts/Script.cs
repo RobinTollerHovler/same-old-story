b3c4c221326af32147e47bb52bba4e0798abf91b
Robin Toller Hovler <robin@colobri.se>
2022-11-12 19:14:57 +0100

    Random sizing for scripts when they are created

--- Assets/Scripts/Objects/Interactables/Scripts/Script.cs
+++ Assets/Scripts/Objects/Interactables/Scripts/Script.cs
@@ -1,12 +1,16 @@
+using System;
 using SameOldStory.Movies;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace SameOldStory.Objects.Interactables.Scripts {
     
     public class Script : InteractableObject {
 
-        private Movie movie;
+        [SerializeField] private Transform node;
         
+        private Movie movie;
+
         public void AssignMovie(Movie movie) {
             this.movie = movie;
             Tooltip = movie.Name;
@@ -15,7 +19,10 @@ namespace SameOldStory.Objects.Interactables.Scripts {
         public void SetXPosition(float position) {
             transform.localPosition = new Vector3(position, transform.localPosition.y, transform.localPosition.z);
         }
-        
+
+        private void OnEnable() {
+            node.localScale = new Vector3(1, 1 + Random.Range(-.2f,.09f), 1);
+        }
     }
     
 }
