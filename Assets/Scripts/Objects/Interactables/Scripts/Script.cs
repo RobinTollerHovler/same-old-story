5ded6a0f323efc11da5aa7487b3f2a2c0d1fe67f
Robin Toller Hovler <robin@colobri.se>
2022-11-12 19:01:41 +0100

    Removed stray DL

--- Assets/Scripts/Objects/Interactables/Scripts/Script.cs
+++ Assets/Scripts/Objects/Interactables/Scripts/Script.cs
@@ -5,7 +5,6 @@ namespace SameOldStory.Objects.Interactables.Scripts {
     public class Script : InteractableObject {
 
         public void SetXPosition(float position) {
-            Debug.Log("Setting X: " + position);
             transform.localPosition = new Vector3(position, transform.localPosition.y, transform.localPosition.z);
         }
         
