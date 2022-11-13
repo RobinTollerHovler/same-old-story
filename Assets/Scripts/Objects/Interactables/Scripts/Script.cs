8bb8addcd1837d4fd47877fe78d26efb5016fa1b
Robin Toller Hovler <robin@colobri.se>
2022-11-13 22:01:34 +0100

    Script names are printed on the back

--- Assets/Scripts/Objects/Interactables/Scripts/Script.cs
+++ Assets/Scripts/Objects/Interactables/Scripts/Script.cs
@@ -15,12 +15,15 @@ namespace SameOldStory.Objects.Interactables.Scripts {
         public void AssignMovie(Movie movie) {
             this.movie = movie;
             Tooltip = movie.Name;
+            textMeshProUGUI.text = movie.Name;
         }
         
         public void SetXPosition(float position) {
             transform.localPosition = new Vector3(position, transform.localPosition.y, transform.localPosition.z);
         }
 
+        private void Awake() => textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
+        
         private void OnEnable() {
             node.localScale = new Vector3(1, 1 + Random.Range(-.2f,.09f), 1);
         }
