b2bb638f7846f01a59713c822a3523b5c259fbe0
Robin Toller Hovler <robin@colobri.se>
2022-11-14 21:09:58 +0100

    removed stray debug.logs

--- Assets/Scripts/Objects/Interactables/Posters/Poster.cs
+++ Assets/Scripts/Objects/Interactables/Posters/Poster.cs
@@ -1,6 +1,5 @@
 using SameOldStory.Movies;
 using SameOldStory.Objects.Interactables.Posters.Components;
-using UnityEngine;
 
 namespace SameOldStory.Objects.Interactables.Posters {
     
@@ -11,13 +10,11 @@ namespace SameOldStory.Objects.Interactables.Posters {
         protected PosterNode PosterNode { get; private set; }
         
         public void AssignMovie(Movie movie) {
-            Debug.Log("Assigning");
             if (movie == null) return;
             posterMovieTitleText?.Set(movie.Name);
         }
 
         protected virtual void Awake() {
-            Debug.Log("Awaking");
             posterMovieTitleText = GetComponentInChildren<PosterMovieTitleText>();
             PosterNode = GetComponentInChildren<PosterNode>();
         }
