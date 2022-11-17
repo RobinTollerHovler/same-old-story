89f0060aa80e589e31058b712c953337005e71b6
Robin Toller Hovler <robin@colobri.se>
2022-11-17 14:30:19 +0100

    Added font data type

---
+++ Assets/Scripts/Core/Data/Font.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+namespace SameOldStory.Core.Data {
+    
+    [CreateAssetMenu(fileName = "New Font", menuName = "Data/Font")]
+    public class Font : UnlockableStudioData {
+
+        [Header("Font options")]
+        [SerializeField] private string fontName;
+        [SerializeField] private TMP_FontAsset tmpFontAsset;
+
+        public string Name => fontName;
+
+    }
+    
+}
