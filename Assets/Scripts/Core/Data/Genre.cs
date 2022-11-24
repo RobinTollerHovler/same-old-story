f485b338e6b789ad189deb8b24af12bcd5300368
Robin Toller Hovler <robin@colobri.se>
2022-11-24 15:11:25 +0100

    Added "Indefinite Article" & "Plural" fields for genres

--- Assets/Scripts/Core/Data/Genre.cs
+++ Assets/Scripts/Core/Data/Genre.cs
@@ -7,12 +7,16 @@ namespace SameOldStory.Core.Data {
         
         [Header("Genre options")]
         [SerializeField] private string genreName;
-        [SerializeField] private string subjectiveTitle;
+        [SerializeField] private string indefiniteArticle;
+        [SerializeField] private string noun;
+        [SerializeField] private string plural;
         [SerializeField] private float attractiveness;
         [SerializeField] private int leastMonthsOfWorkRequired;
         
         public string Name => genreName;
-        public string SubjectiveTitle => subjectiveTitle;
+        public string IndefiniteArticle => indefiniteArticle;
+        public string Plural => plural;
+        public string Noun => noun;
         public float Attractiveness => attractiveness;
         public int LeastMonthsOfWorkRequired => leastMonthsOfWorkRequired;
 
