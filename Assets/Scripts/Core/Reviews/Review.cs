f75c41005932ba1bc421645c3df3519e0524fa47
Robin Toller Hovler <robin@colobri.se>
2022-11-25 19:44:00 +0100

    Assigning movie to review instance

--- Assets/Scripts/Core/Reviews/Review.cs
+++ Assets/Scripts/Core/Reviews/Review.cs
@@ -3,11 +3,16 @@ using SameOldStory.Core.Movies;
 namespace SameOldStory.Core.Reviews {
     
     public abstract class Review {
-
-        protected string review;
-
-        protected Review(Movie movie) {}
         
+        protected string review;
+        protected readonly Movie movie;
+
+        protected Review(Movie movie) {
+            this.movie = movie;
+        }
+
+        public float Score { get; protected set; }
+
         public string Get => review;
 
     }
