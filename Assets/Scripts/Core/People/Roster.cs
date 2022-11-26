051e722109247bc16be6434631ba62cc012aff57
Robin Toller Hovler <robin@colobri.se>
2022-11-26 23:51:10 +0100

    Added event for adding people to roster

--- Assets/Scripts/Core/People/Roster.cs
+++ Assets/Scripts/Core/People/Roster.cs
@@ -1,15 +1,24 @@
+using System;
 using System.Collections.Generic;
 
 namespace Core.People {
     
     public class Roster {
 
+        public event Action<Person> onAddedPersonToRoster;
+        
         private HashSet<Person> persons = new();
 
         public Roster(int initialNumberOfPeople) {
             for (int n = 0; n < initialNumberOfPeople; n++) {
-                persons.Add(new Person());
+                AddPersonToRoster();
             }
+        }
+
+        public void AddPersonToRoster() {
+            Person p = new Person();
+            persons.Add(p);
+            onAddedPersonToRoster?.Invoke(p);
         }
 
     }
