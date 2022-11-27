446456751b1a68608ef394e07097dd40eae3ca37
Robin Toller Hovler <robin@colobri.se>
2022-11-27 22:02:16 +0100

    Added virtual OnOpen method

--- Assets/Scripts/UI/Windows/Window.cs
+++ Assets/Scripts/UI/Windows/Window.cs
@@ -8,6 +8,7 @@ namespace SameOldStory.UI.Windows {
         private WindowActivationNode windowActivationNode;
 
         public abstract void Submit();
+        public virtual void OnOpen() {}
 
         private void Awake() {
             windowActivationNode = GetComponentsInChildren<WindowActivationNode>(true).FirstOrDefault();
@@ -16,8 +17,12 @@ namespace SameOldStory.UI.Windows {
 
         public void Close() => windowActivationNode?.Deactivate();
         protected virtual void SetUp() {}
-        protected void Open() => windowActivationNode?.Activate();
-
+        
+        protected void Open() {
+            windowActivationNode?.Activate();
+            OnOpen();
+        }
+        
     }
     
 }
