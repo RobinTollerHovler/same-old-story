b17f1a730021675cca0434f636552619e8e3c34c
Robin Toller Hovler <robin@colobri.se>
2022-11-28 21:11:00 +0100

    Added month constant

--- Assets/Scripts/Core/Time/Cycle.cs
+++ Assets/Scripts/Core/Time/Cycle.cs
@@ -5,6 +5,8 @@ namespace SameOldStory.Core.Time {
     
     public class Cycle : MonoBehaviour {
 
+        public const float MONTH = 8;
+        
         public static event Action<float> onTick;
 
         private void Update() => onTick?.Invoke(UnityEngine.Time.deltaTime);
