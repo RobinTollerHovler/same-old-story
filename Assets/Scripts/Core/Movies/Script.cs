16391792c8519773feb94777153b0aeec1645f81
Robin Toller Hovler <robin@colobri.se>
2022-11-28 21:11:32 +0100

    Adding roles add months to write-time

--- Assets/Scripts/Core/Movies/Script.cs
+++ Assets/Scripts/Core/Movies/Script.cs
@@ -1,5 +1,6 @@
 using System;
 using Core.Roles;
+using SameOldStory.Core.Time;
 
 namespace Core.Movies {
     
@@ -10,14 +11,18 @@ namespace Core.Movies {
         public event Action onRolesUpdated;
         
         public static Script CurrentlyCreating { get; private set; }
+        
+        public float TimeToWrite { get; private set; }
 
         public static void Create() {
             CurrentlyCreating = new Script();
             onRequestCreateNewScript?.Invoke();
+            CurrentlyCreating.TimeToWrite = Cycle.MONTH;
         }
 
         public void AddRole(Role role) {
             Roles.Add(role);
+            TimeToWrite += Cycle.MONTH * 2;
             onRolesUpdated?.Invoke();
         }
         
