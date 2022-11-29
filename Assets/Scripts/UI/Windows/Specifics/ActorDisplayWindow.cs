988b22c13ea95b91633f61eb1804d3f9c70f5291
Robin Toller Hovler <robin@colobri.se>
2022-11-29 16:46:59 +0100

    Displays correct actor information

--- Assets/Scripts/UI/Windows/Specifics/ActorDisplayWindow.cs
+++ Assets/Scripts/UI/Windows/Specifics/ActorDisplayWindow.cs
@@ -1,13 +1,25 @@
 using System;
 using Core.People;
 using SameOldStory.Input.Mouse;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SameOldStory.UI.Windows.Specifics {
     
     public class ActorDisplayWindow : MonoBehaviour {
 
         private WindowActivationNode windowActivationNode;
+        
+        [SerializeField] private Image face;
+        [SerializeField] private Image eyes;
+        [SerializeField] private Image nose;
+        [SerializeField] private Image mouth;
+        [SerializeField] private Image hair;
+        [SerializeField] private TextMeshProUGUI actorName;
+        [SerializeField] private TextMeshProUGUI actorWage;
+        [SerializeField] private TextMeshProUGUI actorGoodGenres;
+        [SerializeField] private TextMeshProUGUI actorBadGenres;
 
         private void Awake() => windowActivationNode = GetComponentsInChildren<WindowActivationNode>(true)[0];
         
@@ -24,7 +36,18 @@ namespace SameOldStory.UI.Windows.Specifics {
             GameObject obj = Mouse.ObjectUnderCursor;
             IRepresentActor actor = obj.GetComponentInParent<IRepresentActor>();
             if (actor == null) return;
-            if(actor.Actor != null) windowActivationNode.Activate();
+            if (actor.Actor != null) {
+                windowActivationNode.Activate();
+                actorName.text = actor.Actor.Name;
+                face.sprite = actor.Actor.Face.FaceType;
+                eyes.sprite = actor.Actor.Face.Eyes;
+                nose.sprite = actor.Actor.Face.Nose;
+                mouth.sprite = actor.Actor.Face.Mouth;
+                hair.sprite = actor.Actor.Face.HairStyle;
+                hair.color = actor.Actor?.Face.HairColor ?? Color.white;
+                nose.color = actor.Actor?.Face.HairColor ?? Color.white;
+                face.color = actor.Actor?.Face.SkinTone ?? Color.white;
+            }
             else windowActivationNode.Deactivate();
         }
         
