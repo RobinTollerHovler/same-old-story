ac944fc47fd25266924de67ac3e271b01634d44f
Robin Toller Hovler <robin@colobri.se>
2022-11-30 00:06:30 +0100

    Changed clock display

--- Assets/Scripts/UI/TextDisplays/TimeTextDisplay.cs
+++ Assets/Scripts/UI/TextDisplays/TimeTextDisplay.cs
@@ -16,7 +16,21 @@ namespace SameOldStory.UI.TextDisplays {
                 month = 1;
                 year++;
             }
-            SetText($"Y{year} M{month}");
+            string monthName = month switch {
+                1 => "Jan",
+                2 => "Feb",
+                3 => "Mars",
+                4 => "April",
+                5 => "May",
+                6 => "June",
+                7 => "July",
+                8 => "Aug",
+                9 => "Sep",
+                10 => "Oct",
+                11 => "Nov",
+                12 => "Dec"
+            };
+            SetText($"{monthName} Year:{year}");
         }
     }
     
