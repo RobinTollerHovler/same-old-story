cb01b8db5b2c30dc080ee02c22b5ff2c19c886a8
Robin Toller Hovler <robin@colobri.se>
2022-11-30 21:46:34 +0100

    Role debug

--- Assets/Scripts/Core/Studios/DefaultStudioInitializer.cs
+++ Assets/Scripts/Core/Studios/DefaultStudioInitializer.cs
@@ -1,19 +1,30 @@
+using System.Linq;
+using SameOldStory.Core.Data;
 using UnityEngine;
 
 namespace SameOldStory.Core.Studios {
-    
+
     public class DefaultStudioInitializer : MonoBehaviour {
 
         [SerializeField] private int actorWagePerFameLevel = 0;
         [SerializeField] private int movieProfitBase;
         [SerializeField] private int actorHiringCost;
-        
-        private void Awake() => Studio.InitializeNewStudio(
-            actorWagePerFameLevel, 
-            movieProfitBase,
-            actorHiringCost
-        );
-        
+        [SerializeField] private bool printRoleInfo;
+
+        private void Awake() {
+            Studio.InitializeNewStudio(
+                actorWagePerFameLevel,
+                movieProfitBase,
+                actorHiringCost
+            );
+            if (printRoleInfo) PrintRoleInfo();
+        }
+
+        private void PrintRoleInfo() {
+            foreach (Genre g in Studio.Current.AvailableGenres) {
+                Debug.Log($"GENRE: {g.Name} +{Studio.Current.AvailableRoles.Where(role => role.FittingGenres.Contains(g)).Count()} -{Studio.Current.AvailableRoles.Where(role => role.UnfittingGenres.Contains(g)).Count()}");
+            }
+        }
     }
     
 }
