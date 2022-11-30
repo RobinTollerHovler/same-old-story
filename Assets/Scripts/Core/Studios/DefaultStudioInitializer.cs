944dd50780863500d44af2459d36f4398c00d0c0
Robin Toller Hovler <robin@colobri.se>
2022-11-30 21:36:39 +0100

    Exposed MovieProfitBase

--- Assets/Scripts/Core/Studios/DefaultStudioInitializer.cs
+++ Assets/Scripts/Core/Studios/DefaultStudioInitializer.cs
@@ -5,12 +5,12 @@ namespace SameOldStory.Core.Studios {
     public class DefaultStudioInitializer : MonoBehaviour {
 
         [SerializeField] private int actorWagePerFameLevel = 0;
-        [SerializeField] private int payoutPerScore;
+        [SerializeField] private int movieProfitBase;
         [SerializeField] private int actorHiringCost;
         
         private void Awake() => Studio.InitializeNewStudio(
             actorWagePerFameLevel, 
-            payoutPerScore,
+            movieProfitBase,
             actorHiringCost
         );
         
