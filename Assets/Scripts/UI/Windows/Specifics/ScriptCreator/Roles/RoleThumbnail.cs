4941147f3f9c7a0a24fc8282ab15445bbf183795
Robin Toller Hovler <robin@colobri.se>
2022-12-01 14:41:31 +0100

    Fixed bug causing role thumbnails to display wrong text

--- Assets/Scripts/UI/Windows/Specifics/ScriptCreator/Roles/RoleThumbnail.cs
+++ Assets/Scripts/UI/Windows/Specifics/ScriptCreator/Roles/RoleThumbnail.cs
@@ -24,6 +24,7 @@ namespace SameOldStory.UI.Windows.Specifics {
         private void Awake() {
             node = GetComponentInChildren<AddRoleButtonNode>();
             roleInformationTextComponent = GetComponentInChildren<RoleInformationTextComponent>();
+            Debug.Log(roleInformationTextComponent);
         }
 
         public void Hide() {
