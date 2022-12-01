7cf433bfa21d52acf7d3b8871cb2c45e3e8937a9
Robin Toller Hovler <robin@colobri.se>
2022-12-01 09:02:49 +0100

    Fixed some review typos, added some more

--- Assets/Scripts/Core/Reviews/Review.cs
+++ Assets/Scripts/Core/Reviews/Review.cs
@@ -12,12 +12,15 @@ namespace SameOldStory.Core.Reviews {
         private string[] badGeneralReviews() {
             return new[] {
                 "This sucks",
-                "I coud've made this movie",
+                "I could've made this movie",
                 "Who greenlit this script?",
                 "I'd rather watch moss grow than this",
                 "This is truly the end of cinema",
                 "Amateurish, ill-conceived, bad",
                 "Don't waste 2hrs of your life watching this",
+                "Please, no more",
+                "I have never been so bored in my life",
+                "Horrible idea, from start to finish",
             };
         }
 
@@ -29,7 +32,8 @@ namespace SameOldStory.Core.Reviews {
                 "At least it's not boring",
                 "I'm not gonna say I hate it, but I don't love it either",
                 "Fine, but there's better things to watch",
-                "This film-maker might evetually make something interesting",
+                "This film-maker might eventually make something interesting",
+                "It's ok",
             };
         }
 
@@ -42,12 +46,15 @@ namespace SameOldStory.Core.Reviews {
                 "Perfect cast, perfect script, perfect movie",
                 "This movie is the GOAT",
                 "I'll definitely see this many more times",
+                "My new favourite!",
+                "Instant classic!",
+                "Wow, just wow"
             };
         }
 
         private string[] goodRoleMatchReviews(Role role) {
             return new[] {
-                $"It's good to have a {role.RoleTitle} in a {movie.Genre.Noun}",
+                $"It's good to have {role.IndefiniteArticle} {role.RoleTitle} in a {movie.Genre.Noun}",
                 $"Any good {movie.Genre.Noun} should contain {role.IndefiniteArticle} {role.RoleTitle}",
                 $"{movie.Genre.Plural} should contain {role.Plural}",
                 $"What would {movie.Genre.Plural} be without {role.Plural}?",
@@ -77,8 +84,8 @@ namespace SameOldStory.Core.Reviews {
         private string[] badActorMatchReviews(Actor actor) {
             return new[] {
                 $"{actor.Name} is horrible in {movie.Genre.Plural}",
-                $"Fire whoever decided to put {actor.Name} in a {movie.Genre.Noun}",
-                $"Let's just say {actor.Name} should never work in {movie.Genre.Plural} ever again",
+                $"Fire whoever decided to put {actor.Name} in {movie.Genre.IndefiniteArticle} {movie.Genre.Noun}",
+                $"Let's just say {actor.Name} should never star in {movie.Genre.Plural} ever again",
             };
         }
 
