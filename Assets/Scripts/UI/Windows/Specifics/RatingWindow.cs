94b7476e7fe557d560bd263c125c9634af24b893
Robin Toller Hovler <robin@colobri.se>
2022-12-01 15:31:56 +0100

    Rating window title is now dynamic

--- Assets/Scripts/UI/Windows/Specifics/RatingWindow.cs
+++ Assets/Scripts/UI/Windows/Specifics/RatingWindow.cs
@@ -1,13 +1,18 @@
 using SameOldStory.Core;
 using SameOldStory.Core.Movies;
 using SameOldStory.UI.Components.Sliders;
+using TMPro;
+using UnityEngine;
 
 namespace SameOldStory.UI.Windows.Specifics {
     
     public class RatingWindow : Window, IRepresentMovie {
 
+        [SerializeField] private TextMeshProUGUI windowTitle;
+        
         private void OpenWindowForMovie(Movie movie) {
             Movie = movie;
+            windowTitle.text = $"Rating: {movie.Title}";
             MovieRating movieRating = GetComponentInChildren<MovieRating>();
             if(movieRating != null) movieRating.TriggerManualUpdate();
             Open();
