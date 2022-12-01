21a2270580ac946ea25562a49f37a7282ffc9fd7
Robin Toller Hovler <robin@colobri.se>
2022-12-01 20:31:19 +0100

    Tutorial

---
+++ Assets/Scripts/UI/Windows/Specifics/TutorialWindow.cs
@@ -0,0 +1,23 @@
+using System;
+using SameOldStory.Core.Time;
+
+namespace SameOldStory.UI.Windows.Specifics {
+    
+    public class TutorialWindow : Window {
+        
+        private void OnEnable() {
+            Cycle.Pause();
+        }
+
+        protected override void SetUp() {
+            base.SetUp();
+            Open();
+        }
+
+        public override void Submit() {
+            Cycle.Resume();
+        }
+        
+    }
+    
+}
