572d8b369a01d2a930d0c05d213df06edad042c2
Robin Toller Hovler <robin@colobri.se>
2022-12-01 11:39:40 +0100

    Wallet will display red text when below 0

--- Assets/Scripts/UI/TextDisplays/WalletTextDisplay.cs
+++ Assets/Scripts/UI/TextDisplays/WalletTextDisplay.cs
@@ -1,9 +1,13 @@
 using System;
 using SameOldStory.Core.Studios;
+using UnityEngine;
 
 namespace SameOldStory.UI.TextDisplays {
     
     public class WalletTextDisplay : TextDisplay {
+
+        [SerializeField] private Color positiveColor;
+        [SerializeField] private Color negativeColor;
         
         private void OnEnable() {
             Studio.onStudioChanged += TrackStudioWallet;
@@ -23,6 +27,7 @@ namespace SameOldStory.UI.TextDisplays {
         private void UpdateDisplay() {
             if (Studio.Current == null) return;
             int currentBalance = (int)Studio.Current.Wallet.Balance;
+            SetColor(currentBalance < 0 ? negativeColor : positiveColor);
             string balance = currentBalance > 0 ? $"${currentBalance:N0}" : $"-${Math.Abs(currentBalance):N0}";
             SetText($"{balance}");
         }
